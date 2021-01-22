fd952aedf4fe2e06e76d43a97b8ef91d3762abdf
annshiv <annamalaipalani11@gmail.com>
2021-01-22 09:37:48 +0530

    script update

---
+++ Drink water/script.js
@@ -0,0 +1,27 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.getElementById('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+smallCups.forEach((cup,idx) => {
+    cup.addEventListener('click',() => highlightCups(idx))
+})
+
+function highlightCups(idx) {
+    if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')){
+        idx--
+    }
+    smallCups.forEach((cup,idx2) => {
+        if(idx2 <= idx){
+            cup.classList.add('full')
+        } else{
+            cup.classList.remove('full')
+        }
+    })
+    updateBigcup()
+}
+
+function updateBigcup() {
+    const fullCups = document.querySelectorAll('.cup-small,full').length
+    const totalCups = smallCups.length
+}
