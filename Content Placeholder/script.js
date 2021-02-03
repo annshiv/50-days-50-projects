c3148eea27c61fc91d5b1a8feb1ccbada339f454
annshiv <annamalaipalani11@gmail.com>
2021-02-03 09:41:47 +0530

    Content Place Holder

--- Content Placeholder/script.js
+++ Content Placeholder/script.js
@@ -12,14 +12,14 @@ setTimeout(getData, 2500)
 
 function getData() {
   header.innerHTML =
-    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
-  title.innerHTML = 'Lorem ipsum dolor sit amet'
+    '<img src="20210202_102115.jpg" alt="" />'
+  title.innerHTML = 'About'
   excerpt.innerHTML =
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
+    'Beginner in software developer. Practicing & Upgrading my skills in python, public speaking, soft skills, datastructures and web development.'
   profile_img.innerHTML =
-    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
+    '<img src="Document from Annshiv.jpg" alt="" />'
   name.innerHTML = 'Annamalai palani'
-  date.innerHTML = 'Oct 08, 2020'
+  date.innerHTML = 'Full stack developer'
 
   animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
   animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
